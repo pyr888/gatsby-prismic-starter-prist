a699317ca9a1d4232bb92f7a7f65bde5972b61e5
pyr888 <py.rousseau@matchframe.fr>
2021-04-21 23:22:59 +0200

    add prismic account

--- prismic-config.js
+++ prismic-config.js
@@ -1,4 +1,4 @@
 module.exports = {
   // -- Prismic repository GraphQL endpoint
-  apiEndpoint: 'https://your-repo-name.prismic.io/graphql',
+  apiEndpoint: 'https://starter-gatsby-prismic-py.prismic.io/graphql',
 };
